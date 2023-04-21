4bfeeed0ca8d9e4e91e8d7718731b4f2b5fcb733
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-04-20 17:39:08 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * Return: Return(0) (Done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
