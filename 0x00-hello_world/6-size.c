5bb840062a549fe9bf32faacd6cdd56f630f0f67
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-04-20 18:44:32 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  * main - a C program that prints the size of various types on the computer it is compiled and run on
  * Return: Return (0) done
  */
+
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
