87cbad3f365cb5ba21b0d05a8c7fd03b427bf46a
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-04-20 18:15:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A program that prints a line accurately using printf function
  * Return: Return(0) done
 */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
