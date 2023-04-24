6a87bd9feb864cffdc80d8911695c4f061f2dff6
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-04-24 15:50:31 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Return(0) on success
+ */
+int main(void)
+{
+	int s = 97, e = 122;
+	while (s <= e)
+	l{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0)
+}
