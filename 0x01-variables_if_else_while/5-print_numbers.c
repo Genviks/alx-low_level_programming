72d337cbcdef0f5fd503e30181f8d8bb638ba4e6
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-04-24 17:08:34 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main functions
+ *
+ * Return: Return (0) on success
+ */
+def main(void);
+	for i in range(10);
+	print(i)
+
+	if _ _name_ _ == '_ _main_ _';
+	main()
