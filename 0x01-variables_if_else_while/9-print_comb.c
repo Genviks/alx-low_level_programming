168d796714f660e84ebc3f93f0557ff4666f3eac
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-04-24 18:28:46 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * Retutn: Return (0) on success
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(',');
+	putchar(',');
+	}
+	}
+	putchar('\n\)
+
+	return (0);
+
+}
