8724750ae1088788d732808ea62fdaa12569cfe6
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-05-24 18:51:47 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
  * Return: Always 0
  */
 
-int main(int __attribute__((unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -42,4 +42,3 @@ int main(int __attribute__((unused__)) argc, char *argv[])
 
 	return (0);
 }
-
