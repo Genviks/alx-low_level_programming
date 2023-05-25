947a8e5e2532b4e320b8cf816a3b66cbfed27aab
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-05-24 19:02:36 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,15 +23,16 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	int i = 0;
+
 	while (ops[i].op != NULL)
+
 	{
 
 		if (strcmp(ops[i].op, s) == 0)
-		return ops[i].f;
+		return (ops[i].f);
 
 	       i++;
 	}
 
-	
-	return NULL;
+	return (NULL);
 }
