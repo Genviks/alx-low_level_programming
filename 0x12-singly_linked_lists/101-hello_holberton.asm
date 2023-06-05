3bca31c197cf18ad30919b8e42dd4e15a2576fab
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-06-04 18:58:10 -0700

    a 64-bit program

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,25 @@
+section .data
+format db 'Hello, Holberton', 0
+
+section -text
 global main
 extern printf
+
 main:
+; save base pointer and set up stack frame
+push rbp
+mov rbp, rsp
+
+; Pass format string address as argument to printf
 mov edi, format
 xor eax, eax
+xor eax, eax ; Clear eax register (optional)
 call printf
+
+; Clean up stack frame and restore base pointer
+mov rsp, rbp
+pop rbp
+
+; Exit program
 mov eax, 0
 ret
-format: db 'Hello, Holberton\n',0
