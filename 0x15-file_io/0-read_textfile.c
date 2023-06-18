ceee67cf1b258999bc33aab7dcc1fc79ef34d76b
Genevieve Onwuka <genevievechigoziri@gmail.com>
2023-06-17 21:04:30 -0700

    reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char buffer[1024];
 	ssize_t total_read = 0;
-	ssize_t num_read;
+	ssize_t num_read, num_written;
 
 	if (filename == NULL)
 		return (0);
@@ -27,14 +27,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	while (total_read < (ssize_t)letters && (num_read = read(fd,
 					buffer, sizeof(buffer))) > 0)
 	{
-		ssize_t num_written = write(STDOUT_FILENO, buffer, num_read);
+		num_written = write(STDOUT_FILENO, buffer, (size_t)num_read);
 
-		if (num_written == -1 || num_written != num_read)
+		if (num_written == -1 || (size_t)num_written != (size_t)num_read)
 		{
 			close(fd);
 				return (0);
 		}
-		total_read += num_read;
+		total_read += num_written;
 	}
 
 	close(fd);
