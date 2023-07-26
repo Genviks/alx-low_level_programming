6cd415703ab93e68a202219d5f5b5f3542b203c8
Genevieve Onwuka <genevievechigoziri@gmail.com>
2023-07-26 07:37:15 -0700

    Write a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,11 @@
 
 /**
  * _strcmp - function that compares two strings.
- * @s1: valur to input
- * @s2: value to input
+ * @s1: the first string to compare
+ * @s2: the second string to compare
  *
- * @Return: s1[i] - s2[i]
+ * Return: an integer less than, equal to, or greater than zero if s1 is found
+ * respectively, to be less than, to match or be greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,10 +14,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+
 		if (s1[i] != s2[i])
-		{
+	{
 			return (s1[i] - s2[i]);
-		}
+	}
 		i++;
 	}
 
